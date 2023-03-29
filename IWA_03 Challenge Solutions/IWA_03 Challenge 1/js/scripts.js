b5d5c06229ebac9217ccdbff5abe19d6a3489cc6
Olga Maseng <masengolga@gmail.com>
2023-03-29 13:49:37 +0200

    Organising files

--- IWA_03 Challenge Solutions/IWA_03 Challenge 1/js/scripts.js
+++ IWA_03 Challenge Solutions/IWA_03 Challenge 1/js/scripts.js
@@ -6,7 +6,7 @@ import { company } from './configuration.js';
 
 import { year } from './configuration.js';
 
-console.log(company, year)
+
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
