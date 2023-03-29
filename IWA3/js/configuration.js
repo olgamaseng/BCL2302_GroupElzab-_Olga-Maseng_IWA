bc9f21563395cc7dcfead96558e0f130a0b38064
Olga Maseng <masengolga@gmail.com>
2023-03-29 13:43:43 +0200

    Completing IWA3 challenge1

--- IWA3/js/configuration.js
+++ IWA3/js/configuration.js
@@ -2,5 +2,6 @@
 
 // configuration.js
 
-const company = 'ACME Inc.';
-const year = 2022;
+export const company = 'ACME Inc.';
+
+export const year = 2022;
--- IWA3/js/scripts.js
+++ IWA3/js/scripts.js
@@ -2,9 +2,11 @@
 
 // scripts.js
 
-import { company } from '../js';
+import { company } from './configuration.js';
 
 import { year } from './configuration.js';
+
+console.log(company, year)
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
