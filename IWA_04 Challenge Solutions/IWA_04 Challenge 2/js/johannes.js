34f453f2783169478293c33a57439a5ed9307565
Olga Maseng <masengolga@gmail.com>
2023-03-30 13:23:38 +0200

    Working on IWA 04 challenge

--- IWA_04 Challenge Solutions/IWA_04 Challenge 2/js/johannes.js
+++
@@ -1,8 +0,0 @@
-// johannes.js
-
-const firstname = "Johannes"
-const surname = "Potgieter"
-export const role = "Intern"
-
-const display= firstname + " " + surname + " (" + role + ")"
-document.querySelector('#johannes').innerText = display
