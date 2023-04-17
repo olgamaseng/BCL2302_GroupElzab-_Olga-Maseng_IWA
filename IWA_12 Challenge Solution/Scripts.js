b2be0473cb7eb44091361aaa1732234b9df1af50
Olga Maseng <masengolga@gmail.com>
2023-04-17 13:27:55 +0200

    Editing IWA 12 and Working on IWA 13

--- IWA_12 Challenge Solution/Scripts.js
+++ IWA_12 Challenge Solution/Scripts.js
@@ -1,70 +1,5 @@
 // scripts.js
 
-/*const STATUS_MAP = {
-    shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    },
-    checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    }
-}
-
-// Edit below line 
-
-const book1 = document.getElementById('book1');
-status1 = book1.querySelector('#status');
-reserve1 = book1.querySelector('#reserve');
-checkout1 = book1.querySelector('#checkout');
-checkin1 = book1.querySelector('#checkin');
-
-const book2 = document.getElementById('book2');
-status2 = book2.querySelector('#status');
-reserve2 = book2.querySelector('#reserve');
-checkout2 = book2.querySelector('#checkout');
-checkin2 = book2.querySelector('#checkin')
-
-const book3 = document.getElementById('book3');
-status3 = book3.querySelector('#status')
-reserve3 = book3.querySelector('#reserve')
-checkout3 = book3.querySelector('#checkout')
-checkin3 = book3.querySelector('#checkin')
-
-checkin1.color = none
-status1.style.color = STATUS_MAP.overdue.color ? 'black' : 'gray'
-reserve1 = STATUS_MAP.canReserve ? 'true' : 'false'
-checkout1 = STATUS_MAP.canCheckout ? 'true' : 'false'
-checkin1 = STATUS_MAP.canCheckIn ? 'true' : 'false'
-
-checkin2.color = none
-status2.style.color = STATUS_MAP.reserved.color ? 'black' : 'gray'
-reserve2 = STATUS_MAP.canReserve ? 'enabled' : 'disabled'
-checkout2 = STATUS_MAP.canCheckout ? 'enabled' : 'disabled'
-checkin2 = STATUS_MAP.canCheckIn ? 'enabled' : 'disabled'
-
-checkin3.color = none
-status3.style.color = STATUS_MAP.shelf.color ? 'black' : 'gray'
-reserve3 = STATUS_MAP.shelf.canReserve ? 'enabled' : 'disabled'
-checkout3 = STATUS_MAP.shelf.canCheckout ? 'enabled' : 'disabled'
-checkin3 = STATUS_MAP.shelf.canCheckIn ? 'enabled' : 'disabled'*/
-
 const STATUS_MAP = {
     shelf: {
         color: 'green',
@@ -91,10 +26,9 @@ const STATUS_MAP = {
         canCheckIn: true,
     }
 }
-/*
-* Edit below line
-*________________________________________________________________________________________________________________________________________________
-*/
+
+//Edit below line
+_____________________________________________________________________________________________________________________________________________
 const book1 = {
     status: document.querySelector('#book1 .status'),
     reserve: document.querySelector('#book1 .reserve'),
@@ -118,11 +52,13 @@ const book1 = {
   book1.reserve.disabled = !STATUS_MAP.overdue.canReserve;
   book1.checkout.disabled = !STATUS_MAP.overdue.canCheckout;
   book1.checkin.disabled = !STATUS_MAP.overdue.canCheckIn;
+
   book2.checkin.style.filter = 'grayscale(100%)';
   book2.status.style.color = STATUS_MAP.reserved.color;
   book2.reserve.disabled = !STATUS_MAP.reserved.canReserve;
   book2.checkout.disabled = !STATUS_MAP.reserved.canCheckout;
   book2.checkin.disabled = !STATUS_MAP.reserved.canCheckIn;
+
   book3.checkin.style.filter = 'grayscale(100%)';
   book3.status.style.color = STATUS_MAP.shelf.color;
   book3.reserve.disabled = !STATUS_MAP.shelf.canReserve;
---
+++ IWA_13 Challenge Solution/Scripts.js
@@ -0,0 +1,42 @@
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+//Remove the = after the const and add it as a declaration
+const logCalc = () => {
+    const isString = typeof calculated === 'string' // add == to make it a comparisson operator and remove number and the -
+    const calculatedAsNumber = isString ?  parseInt(calculated) : calculated   // Remove ParseNumber and make it ParseInt instead to be able to change the value from a string to a number and move the calculated after the parseINT so that it is logged after 
+    calculated = calculatedAsNumber + 1 //remove == and make it a declaration instead of a comparisson operator
+}
+
+const calcUser = () => {
+    logCalc ()
+    if (calculated > 2) user = 'John'
+    if (calculated > 2) state = 'requesting'
+    if (calculated > 3) state = 'idle'
+}
+
+const checkUser = () => {
+    if (user && state === 'requesting' && calculated === 3) {  // add calculated and the number of times which is 3 in this case
+        console.log(`User: ${user} (${calculated})`)
+    }
+}
+
+// Only allowed to change code above
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
