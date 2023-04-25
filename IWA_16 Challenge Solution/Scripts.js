840a5a2e04e777bd5dcf4c898848b39a3029e77b
Olga Maseng <masengolga@gmail.com>
2023-04-25 11:32:32 +0200

    IWA _16

--- IWA_16 Challenge Solution/Scripts.js
+++ IWA_16 Challenge Solution/Scripts.js
@@ -1,15 +1,181 @@
+// scripts.js
 
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const MONTHS = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
 
+const data = {
+  response: {
+    requestType: "FETCH_ATHLETE_DATA",
+    requestBy: "ALL_MATCHING_ATHLETES",
+    forDisplay: "BEST_RACES",
 
-function logTwice () {
-  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`);
-}
+    data: {
+      NM372: {
+        firstName: "Nwabisa",
+        surname: "Masiko",
+        id: "NM372",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [9, 7, 8, 6],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [6, 7, 8, 7],
+          },
+        ],
+      },
 
-logTwice();
-logTwice();
+      SV782: {
+        firstName: "Schalk",
+        surname: "Venter",
+        id: "SV782",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [10, 8, 3, 12],
+          },
+          {
+            date: '2022-11-25T20:00:00.000Z',
+            time: [6, 8, 9, 11],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [10, 11, 4, 8],
+          },
+          {
+            date: '2022-12-09T20:00:00.000Z',
+            time: [9, 8, 9, 11],
+          },
+        ],
+      },
+    },
+  },
+};
 
+// Only edit below this comment
 
+// const createHtml = (athlete) => {
+//   {firstName, surname, id, races} = athlete
+//   [date], [time] = races.reverse()
 
+//   const fragment = document.createDocumentFragment();
+
+//   const title = document.createElement('h2');
+//   title= id;
+//   section.appendChild(title);  //removed fragment and replaced with section so that it can be taken from the HTML doc
+
+//   const list = document.createElement('dl');
+
+//   const day = date.getDate();
+//   const month = MONTHS[date.month];
+//   const year = date.year;
+
+//   first, second, third, fourth = timeAsArray;
+//   total = first + second + third + fourth;
+
+//   const hours = total / 60;
+//   const minutes = total / hours / 60;
+
+//   section.innerHTML = /* html */` // removed list and replaced with section
+//     <dt>Athlete</dt>
+//     <dd>${firstName surname}</dd>
+
+//     <dt>Total Races</dt>
+//     <dd>${races}</dd>
+
+//     <dt>Event Date (Latest)</dt>
+//     <dd>${day month year}</dd>
+
+//     <dt>Total Time (Latest)</dt>
+//     <dd>${hours.padStart(2, 0) minutes}</dd>
+//   `;
+
+//   fragment.appendChild(list);
+// }
+
+// [NM372], [SV782] = data
+// document.querySelector(NM372).appendChild(createHtml(NM372));
+// document.querySelector(SV782).appendChild(createHtml(SV782));
+
+// Only edit below this comment
+  //Inserted an object as an input parameter and extracted firstName, surname, id and races using the destructuring assignment
+  //I.e. inserted all the info related to the athlete in the const, so that I can insert it in html as well
+  const createHtml = (athlete) => {
+    const { firstName, surname, id, races } = athlete;
+  
+    const latestRace = races[races.length - 1];
+    const { date, time } = latestRace;
+  //used the array-reverse method in time, so that it could display first - fourth
+    const [fourth, third, second, first] = time.reverse();
+    const total = first + second + third + fourth;
+    const hours = Math.floor(total / 60);
+    //used the modulus operator (%), which returns the remainder of an hour (60Min)
+    const minutes = total % 60;
+
+    const months = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ]; 
+    const dateObj = new Date(date);
+    const day = dateObj.getDate();
+    const monthIndex = dateObj.getMonth();
+    const year = dateObj.getFullYear();
+    
+    const formattedDate = `${months[monthIndex]} ${day}, ${year}`;
+  
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+  
+    const section = document.querySelector(`[data-athlete="${id}"]`);
+    //created an element of (h2) for the id
+    const title = document.createElement('h2');
+    title.textContent = id;
+    section.appendChild(title);
+  
+    const list = document.createElement('dl');
+  
+    const fullnameTerm = document.createElement('dt');
+    fullnameTerm.textContent = 'Full Name';
+    const fullnameDef = document.createElement('dd');
+    fullnameDef.textContent = `${firstName} ${surname}`;
+    list.appendChild(fullnameTerm);
+    list.appendChild(fullnameDef);
+  
+    const racesTerm = document.createElement('dt');
+    racesTerm.textContent = 'Total Races';
+    const racesDef = document.createElement('dd');
+    racesDef.textContent = races.length;
+    list.appendChild(racesTerm);
+    list.appendChild(racesDef);
+  //created the label of the date needed using the (dt) element
+    const latestRaceTerm = document.createElement('dt');
+    latestRaceTerm.textContent = 'Event Date (Latest)';
+    //created the actual value of of the date needed using the (dd) element
+    const latestRaceDef = document.createElement('dd');
+    latestRaceDef.textContent = formattedDate;
+    list.appendChild(latestRaceTerm);
+    list.appendChild(latestRaceDef);
+  
+    const latestTimeTerm = document.createElement('dt');
+    latestTimeTerm.textContent = 'Total Time (Latest)';
+    const latestTimeDef = document.createElement('dd');
+    latestTimeDef.textContent = formattedTime;
+    list.appendChild(latestTimeTerm);
+    list.appendChild(latestTimeDef);
+  //used (appendChild) to synchronize dt and dd
+    section.appendChild(list);
+  };
+  
+  const athletes = Object.values(data.response.data);
+  athletes.forEach((athlete) => createHtml(athlete));
