4c30be44276b5e3f376dae5d54b06d2c808238d5
Olga Maseng <masengolga@gmail.com>
2023-04-27 10:53:31 +0200

    Update Script.js IWA-17

--- IWA_17 Challenge Solution/Scripts.js
+++ IWA_17 Challenge Solution/Scripts.js
@@ -19,155 +19,75 @@ const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 
 
 // Only edit below 
 
-// const createArray = (length) => {
-//   const result = []; 
-
-//   for (let i = 0; i > length; i++ ) { // corrected let i, length to the correct one
-//   result.push(null); //added a for loop for result and pushed the result by using .push()
-//   }
-//   return result;
-// }
-
-// const createData = () => {
-//   const current = new Date();// added () on new date and semi-colons
-//   current = SetDate(1);
-
-//   const startDay = current.getDay();   //added const  and changed day to getDay and also added the missing ()
-//   const daysInMonth = getDaysInMonth(current);  // addedconst
-
-//  const weeks = createArray(5);  //added const
-//   const days = createArray(7);  //added const
-//   let value = null;             // added let is the variable will be used again in the code
-
-//   for (weekIndex in weeks) {
-//       value = [{
-//           week: weekIndex + 1,
-//           days: []
-//       }]
-
-//       for (dayIndex in days) {
-//           value = dayIndex - startDay
-//           isValid = day > 0 && day <= daysInMonth
-
-//           result[weekIndex].days = [{
-//               dayOfWeek: dayIndex + 1,
-//               value: isValid && day,
-//           }]
-//       }
-//   }
-// }
-
-// const addCell = (existing, classString, value) => {
-//   const result = /* html */ `
-//       <td ${classString}>
-//           ${value}
-//       </td>
-
-//       ${existing}
-//   `
-// }
-
-// const createHtml = (data) => {
-//   let result = ''
-
-//   for (week, days in data) {
-//       let inner = ""
-//       inner= addCell(inner, 'table__cell  table__cell_sidebar', 'Week ${week.week}'); // added inner infront of addCell to declare after it was let in the top and added $ infront of {week} to interpolate in properly and added .week inside it
   
-//       for (dayOfWeek, value in days) {
-//           let classString = 'table__cell';   // added '' to make it a string
-//          const isToday = new Date().getDate() === day.value && new Date().getMonth() === new Date().getMonth(); //added () next to newDate and added getDate to get the date
-//          const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7; //added day. and == before 1, removed && and added || added day so that i can acces dayofweek using dot notaion. added an extra = to make it stricter
-//           const isAlternate = week.week % 2 === 0; //added week
-
-//           /*let classString = 'table__cell'*/ //removed this line as it was saying i cant redeclare classString. I used it as a guideline instead so I removed claasString and backtags and replaced with '', and added semi colons.
-
-//           if (isToday) {classString += 'table__cell_today';
-//         }
-
-//           if (isWeekend) {classString += 'table__cell_weekend';}  //added $ and back tigs and removed ''
-
-//           if (isAlternate) {classString += 'table__cell_alternate';}
-
-//           inner = addCell(inner, classString, day.value) //added inner = infront of addCell
-//       }
-
-//       result += `<tr>${inner}</tr>`; // added + sign before the = as it was not supposed to be a declaration and semicolon
-//   }
-//   return result;
-// }
-
-
-
 const createArray = (length) => {
   const result = [];
-  for (let i = 0; i < length; i++) {
-      result.push(i);
+  for (let i = 0; i < length; i++) { // corrected let i, length to the correct one
+      result.push(i); //added a for loop for result and pushed the result by using .push(i)
   }
   return result;
 };
-const createData = function(){
-  const current = new Date();
-  current.setDate(1);
-  const startDay = current.getDay();
-  const daysInMonth = getDaysInMonth(current);
-  const weeks = createArray(6);
-  const days = createArray(7);
-   let result = [] //value = null
+const createData = function(){ //added function and removed the arrow function =>
+  const current = new Date();  // added () on new date and semi-colons
+  current.setDate(1);  //removed = and put a .
+  const startDay = current.getDay(); //added const  and changed day to getDay and also added the missing ()
+  const daysInMonth = getDaysInMonth(current);  // added const
+  const weeks = createArray(6); //changed the value to 6 so that I can be able to see the last number and added const
+  const days = createArray(7);  //added const
+   let result = [] //value = null  // added let because the variable will be used again in the code
   for (let weekIndex = 0; weekIndex < weeks.length; weekIndex++) {
-      let value = {
+      let value = {       //removed opening [ and added let
           week: weekIndex + 1,
           days: []
-      };
+      };             // removed closing square ]
       for (let dayIndex = 0; dayIndex < days.length; dayIndex++) {
-          const day = (weekIndex * 7) + dayIndex - startDay + 1;
-          const isValid = day > 0 && day <= daysInMonth;
-          let classString = 'table__cell';
+          const day = (weekIndex * 7) + dayIndex - startDay + 1;   //removed value and added const day
+          const isValid = day > 0 && day <= daysInMonth; //added const
+          let classString = 'table__cell'; //moved this line to the right position as it was placed in the wrong position. I removed  backtags and replaced with '', and added semi colons.
           if (dayIndex === 0 || dayIndex === 6) {
               classString += 'table__cell_weekend';
           }
-           value.days.push({
+           value.days.push({ //removed [ and replaced with (,removed =
               dayOfWeek: dayIndex + 1,
-              value: isValid ? day : '',
+              value: isValid ? day : '',  //removed && and added ? instead 
               class: classString,
-           });
+           });  //removed closing ] and replaced with ) 
           }
           weeks[weekIndex] = value;
       }
       return weeks;
 };
-const addCell = function(existing, classString, value) {
+const addCell = function(existing, classString, value) {   //removed => and replaced with function
   return `${existing}<td class="${classString}">${value}</td>`;
-};
-const createHtml = function(data) {
+};  //moved existing from the bottom to the top and added class =.,removed const result and =, and replaced with return also added semicolon 
+const createHtml = function(data) {  //removed => and wrote function instead
   let result = '';
   for (let i = 0; i < data.length; i++) {
-    const week = data[i];
+    const week = data[i];   // added const and removed ,days in and replaced with =
     let inner = '';
-    inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week.week}`);
+    inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week.week}`);   // added inner infront of addCell to declare after it was let in the top and added $ infront of {week} and used `` to interpolate in properly and added .week inside it
     for (let j = 0; j < week.days.length; j++) {
-      const day = week.days[j];
-      let classString = 'table__cell';
-      const currentDate = new Date();
-      const isToday = currentDate.getDate() === day.value && currentDate.getMonth() === currentDate.getMonth();
-      const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7;
-      const isAlternate = week.week % 2 === 0;
+      const day = week.days[j];     //removed , value in days and replaced with . and added const before day and separated dayofWeek, removed of and replaced with =
+      let classString = 'table__cell';  // added '' to make it a string
+      const currentDate = new Date();  // declared current date to be new date
+      const isToday = currentDate.getDate() === day.value && currentDate.getMonth() === currentDate.getMonth();  //added currentDate and added getDate to get the date and getMonth to get the month
+      const isWeekend = day.dayOfWeek === 1 || day.dayOfWeek === 7;  //added day. and == before 1, removed && and added || added day so that i can acces dayofweek using dot notaion. added an extra = to make it stricter
+      const isAlternate = week.week % 2 === 0; //added week
       if (isToday) {
-        classString = `${classString} table__cell_today`;
+        classString = `${classString} table__cell_today`;   //added {} and semicolons on the three if statements
       }
       if (isWeekend) {
-        classString = `${classString} table__cell_weekend`;
+        classString = `${classString} table__cell_weekend`;   //added $ and back tags and removed '' and ==
       }
       if (isAlternate) {
         classString = `${classString} table__cell_alternate`;
       }
-      inner = addCell(inner, classString, day.value || '');
+      inner = addCell(inner, classString, day.value || '');  //added inner = infront of addCell
     }
-    result += `<tr>${inner}</tr>`;
+    result += `<tr>${inner}</tr>`;  // added + sign before the = as it was not supposed to be a declaration and semicolon
   }
   return result;
 };
-
 
 // Only edit above
 
@@ -176,3 +96,4 @@ document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]
 
 const data = createData()
 document.querySelector('[data-content]').innerHTML = createHtml(data)
+
